cc1b9dbdf375ca16c6958172f8da21945303d5d4
Roberto Dutra <robertodtraemp@gmail.com>
2022-10-10 11:36:17 -0300

    [WIP-DM-20]-JSON de retorno das consultas formatado conforme solicitado

--- backend/medicar/serializer.py
+++ backend/medicar/serializer.py
@@ -1,5 +1,14 @@
+from asyncore import read
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.forms.models import model_to_dict
 from medicar.models import Especialidade, Medico, Horario, Agenda, Consulta
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
 
 
 class EspecialidadeSerializer(serializers.ModelSerializer):
@@ -22,6 +31,7 @@ class HorarioSerializer(serializers.ModelSerializer):
 
 class AgendaSerializer(serializers.ModelSerializer):
     horarios = HorarioSerializer(read_only=True, many=True)
+    medico = MedicoSerializer(many=False, read_only=True)
 
     class Meta:
         model = Agenda
@@ -30,7 +40,26 @@ class AgendaSerializer(serializers.ModelSerializer):
 
 
 class ConsultaSerializer(serializers.ModelSerializer):
+    medico = serializers.SerializerMethodField()
+    dia = serializers.SerializerMethodField()
+    horario = serializers.SerializerMethodField()
+    data_agendamento = serializers.DateTimeField(source='created_at')
+
     class Meta:
         model = Consulta
-        fields = ['id', 'agenda', 'horario',
-                  'paciente', 'created_at', 'updated_at']
+        fields = [
+            'id',
+            'dia',
+            'horario',
+            'data_agendamento',
+            'medico'
+        ]
+
+    def get_medico(self, obj):
+        return (model_to_dict(obj.agenda.medico))
+
+    def get_dia(self, obj):
+        return (obj.agenda.dia)
+
+    def get_horario(self, obj):
+        return (obj.horario.horario)
