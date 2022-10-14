720eaee723678b8ce165f44a646301d5f5fef3a6
Roberto Dutra <robertodtraemp@gmail.com>
2022-10-13 23:00:09 -0300

    [Closes MD-28]-Roteamento do frontend configurado

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { SignupScreenComponent } from './signup-screen/signup-screen.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginScreenComponent,
+  },
+  { path: 'home', component: HomeScreenComponent },
+  { path: 'signup', component: SignupScreenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
